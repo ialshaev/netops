48987a3dd6b54938bec8a1d5646e72e7e2f7085c
Ilia <alshaev51@gmail.com>
2022-02-09 15:02:04 +0300

    git learning

--- script01.py
+++ script01.py
@@ -1,6 +1,6 @@
 import this;
 
-dict01 = {'name': 'Ilia', 'age': '26', 'address': 'Yakubovicha 24'}
+dict01 = {'name': 'name', 'age': '10', 'address': 'street'}
 
 print(dict01.get('name'))
 print(dict01.get('age'))
