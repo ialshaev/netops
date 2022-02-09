b27098a46b2ad779712283280de88357c58c33c8
ialshaev <alshaev51@gmail.com>
2022-02-09 15:04:24 +0300

    add automation scripts

---
+++ get_vlans_list.py
@@ -0,0 +1,40 @@
+#!/home/admililiaa/netprog/bin/python3
+import requests
+import json
+from pprint import pprint
+from rich.console import Console
+from rich.table import Table
+
+url = "http://192.168.246.130:8000/api/ipam/vlans/"
+headers = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+  'Authorization': 'Token 0123456789abcdef0123456789abcdef01234567'
+}
+
+vlans_output = requests.request("GET", url, headers=headers).json()
+vlans_number = len(vlans_output['results'])
+print(f'The cureent VLAN number is: {vlans_number}')
+
+# table_vlans = Table(title="VLANS")
+# table_vlans.add_column("VLAN ID", justify="center", style="cyan", no_wrap=True)
+# table_vlans.add_column("NAME", style="magenta")
+# table_vlans.add_column("DESCRIPTION", style="green")
+# for v in nb.ipam.vlans.filter(site=site):
+#     table_vlans.add_row(str(v.vid), v.name, v.description)
+# console = Console()
+# console.print(table_vlans)
+
+vlans = {}
+descriptions = {}
+for i in range (vlans_number):
+    pprint(vlans_output['results'][i]['name'])
+    pprint(vlans_output['results'][i]['description'])
+    pprint(vlans_output['results'][i]['site']['name'])
+    pprint('---------')
+
+# for i in range (vlans_number):    
+#     vlans[i] = "vlans_output['results'][i]['name']"
+#     descriptions[i] = "vlans_output['results'][i]['description']"
+# print(vlans)
+# print(descriptions)
