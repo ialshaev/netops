968f82a00053166c8b269a1c87d76ae40c3cd7c1
ialshaev <alshaev51@gmail.com>
2022-02-10 20:10:08 +0300

    add functions; small fixes

--- readme.txt
+++ readme.txt
@@ -1,40 +1,76 @@
 the switchconfig.py execution result:
 
-15:52 $ ./add_vlan_on_sw.py 
+19:59 $ ./switchconfig.py 
 'RETREIVING DEVICE INFORMATION'
+'number of devices defined in NetBox: 5'
 'GENERATING IP ADDRESS LIST'
 ("The list of ip addresses: ['192.168.246.241', '192.168.246.242', "
  "'192.168.246.243', '192.168.246.244', '192.168.246.245']")
 '--------------------------------'
-'The VLAN number is: 11'
-                             VLANs                              
-┏━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━┓
-┃ VLAN ID ┃ NAME               ┃ DESCRIPTION            ┃ SITE ┃
-┡━━━━━━━━━╇━━━━━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━━━━━━━━━╇━━━━━━┩
-│   101   │ SPB-MGMT           │ network management     │ SPB  │
-│   102   │ SPB-IPMI           │ server management      │ SPB  │
-│   103   │ SPB-TRANSIT        │ transit connectivity   │ SPB  │
-│   104   │ SPB-INFRASTRUCTURE │ infrastructure servers │ SPB  │
-│   105   │ SPB-ENGINEERS      │ engeneering stuff      │ SPB  │
-│   106   │ SPB-USERS-1FLOOR   │ data                   │ SPB  │
-│   107   │ SPB-USERS-2FLOOR   │ data                   │ SPB  │
-│   108   │ SPB-USERS-3FLOOR   │ data                   │ SPB  │
-│   109   │ SPB-QA             │ QA data                │ SPB  │
-│   110   │ STO-QA             │ QA data                │ STO  │
-│   111   │ STO-MGMT           │ network management     │ STO  │
-└─────────┴────────────────────┴────────────────────────┴──────┘
-'GENERSTING VLAN LISTS'
+'GENERATING VLAN TABLE'
+'number of VLANs defined in NetBox: 11'
+                           VLANs                            
+┏━━━━━┳━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━┓
+┃ VID ┃ NAME               ┃ DESCRIPTION            ┃ SITE ┃
+┡━━━━━╇━━━━━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━━━━━━━━━╇━━━━━━┩
+│ 101 │ SPB-MGMT           │ network management     │ SPB  │
+│ 102 │ SPB-IPMI           │ server management      │ SPB  │
+│ 103 │ SPB-TRANSIT        │ transit connectivity   │ SPB  │
+│ 104 │ SPB-INFRASTRUCTURE │ infrastructure servers │ SPB  │
+│ 105 │ SPB-ENGINEERS      │ engeneering stuff      │ SPB  │
+│ 106 │ SPB-USERS-1FLOOR   │ data                   │ SPB  │
+│ 107 │ SPB-USERS-2FLOOR   │ data                   │ SPB  │
+│ 108 │ SPB-USERS-3FLOOR   │ data                   │ SPB  │
+│ 109 │ SPB-QA             │ QA data                │ SPB  │
+│ 110 │ STO-QA             │ QA data                │ STO  │
+│ 111 │ STO-MGMT           │ network management     │ STO  │
+└─────┴────────────────────┴────────────────────────┴──────┘
+'GENERATING VLAN LISTS'
 [101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111]
 ['SPB-MGMT', 'SPB-IPMI', 'SPB-TRANSIT', 'SPB-INFRASTRUCTURE', 'SPB-ENGINEERS', 'SPB-USERS-1FLOOR', 'SPB-USERS-2FLOOR', 'SPB-USERS-3FLOOR', 'SPB-QA', 'STO-QA', 'STO-MGMT']
 '--------------------------------'
+'SCANNING FOR IP ADDRESS AVAILABILITY'
+PING 192.168.246.241 (192.168.246.241) 56(84) bytes of data.
+64 bytes from 192.168.246.241: icmp_seq=1 ttl=255 time=2.63 ms
+
+--- 192.168.246.241 ping statistics ---
+1 packets transmitted, 1 received, 0% packet loss, time 0ms
+rtt min/avg/max/mdev = 2.628/2.628/2.628/0.000 ms
+Available
+PING 192.168.246.242 (192.168.246.242) 56(84) bytes of data.
+64 bytes from 192.168.246.242: icmp_seq=1 ttl=255 time=3.19 ms
+
+--- 192.168.246.242 ping statistics ---
+1 packets transmitted, 1 received, 0% packet loss, time 0ms
+rtt min/avg/max/mdev = 3.186/3.186/3.186/0.000 ms
+Available
+PING 192.168.246.243 (192.168.246.243) 56(84) bytes of data.
+64 bytes from 192.168.246.243: icmp_seq=1 ttl=255 time=6.92 ms
+
+--- 192.168.246.243 ping statistics ---
+1 packets transmitted, 1 received, 0% packet loss, time 0ms
+rtt min/avg/max/mdev = 6.924/6.924/6.924/0.000 ms
+Available
+PING 192.168.246.244 (192.168.246.244) 56(84) bytes of data.
+64 bytes from 192.168.246.244: icmp_seq=1 ttl=255 time=3.12 ms
+
+--- 192.168.246.244 ping statistics ---
+1 packets transmitted, 1 received, 0% packet loss, time 0ms
+rtt min/avg/max/mdev = 3.117/3.117/3.117/0.000 ms
+Available
+PING 192.168.246.245 (192.168.246.245) 56(84) bytes of data.
+64 bytes from 192.168.246.245: icmp_seq=1 ttl=255 time=3.86 ms
+
+--- 192.168.246.245 ping statistics ---
+1 packets transmitted, 1 received, 0% packet loss, time 0ms
+rtt min/avg/max/mdev = 3.857/3.857/3.857/0.000 ms
+Available
 'PROVIDE ADMIN CREDENTIALS'
 login: cisco
 password: cisco
 'PUSH VLAN CONFIGURATION'
 'PUSH SWITCHPORT CONFIGURATION'
-SWITCH IP: 192.168.246.244
-'DISPLAY CONFIGURATION FOR 192.168.246.244'
-
+'DISPLAY CONFIGURATION FOR 192.168.246.242'
 
 VLAN Name                             Status    Ports
 ---- -------------------------------- --------- -------------------------------
@@ -56,18 +92,16 @@ VLAN Name                             Status    Ports
 1005 trnet-default                    act/unsup 
 Building configuration...
 
-Current configuration : 174 bytes
+Current configuration : 157 bytes
 !
-interface GigabitEthernet0/2
+interface GigabitEthernet0/3
  description **SPB-INFRASTRUCTURE cfg by netmiko**
  switchport access vlan 104
  switchport mode access
- media-type rj45
  negotiation auto
 end
 
 !!!
 For improvements:
-- use multithreading
-- define functions
-- use main() ?
+- involve multithreading
+- fix functions
--- switchconfig.py
+++ switchconfig.py
@@ -1,96 +1,111 @@
 #!/home/admililiaa/netprog/bin/python3
 import requests
-import json
-import socket
 import os
 from pprint import pprint
 from netmiko import ConnectHandler
 from rich.console import Console
 from rich.table import Table
 
+def push_config_vlan(ip,v,n,username,password):
+    device = ConnectHandler(device_type='cisco_ios', ip=ip, username=username, password=password)
+    vlancli = [f'vlan {v}', f'name {n}']
+    device.send_config_set(vlancli)
+    device.disconnect()
+
+def push_config_switchport(ip,int,username,password):
+    device = ConnectHandler(device_type='cisco_ios', ip=ip, username=username, password=password)
+    portcli = [f'interface {int}', 'description **SPB-INFRASTRUCTURE cfg by netmiko**', 'switchport mode access', f'switchport access vlan 104']
+    device.send_config_set(portcli)
+    device.disconnect()
+
+def check_config(ip,cmds,username,password):
+    for cmd in cmds:
+        device = ConnectHandler(device_type='cisco_ios', ip=ip, username=username, password=password)
+        device.send_command(cmd)
+        device.disconnect()
+
+def rest(url):
+    headers = {'Content-Type': 'application/json','Accept': 'application/json','Authorization': 'Token 0123456789abcdef0123456789abcdef01234567'}
+    result = requests.request("GET", url, headers=headers).json()
+    return(result)
+
+def check_availability(ip):
+    status = os.system('ping -c 1 ' + ip)
+    if status == False:
+        pingresult = 'Available'
+    else:
+        pingresult = 'Unavailable'
+    return(pingresult)
+
 ###Retreive device information from NetBox###
 pprint('RETREIVING DEVICE INFORMATION')
 url_devices = "http://192.168.246.130:8000/api/dcim/devices/"
-url_vlans = "http://192.168.246.130:8000/api/ipam/vlans/"
-headers = {'Content-Type': 'application/json','Accept': 'application/json','Authorization': 'Token 0123456789abcdef0123456789abcdef01234567'}
-device_list = requests.request("GET", url_devices, headers=headers).json()
-n_devices = len(device_list['results'])
-# pprint(f'The device number is: {n_devices}')
+devices = rest(url_devices)
+n_devices = len(devices['results'])
+pprint(f'number of devices defined in NetBox: {n_devices}')
 pprint('GENERATING IP ADDRESS LIST') # create the list of IP addresses
 ip_addr_list = []
 for i in range (n_devices):
-    ip = device_list['results'][i]['primary_ip4']['address']
+    ip = devices['results'][i]['primary_ip4']['address']
     ip_addr_list.append(ip[:-3])
 pprint(f'The list of ip addresses: {ip_addr_list}') # pprint(type(ip_addr_list[0])) --> check for the list element type, must be string
 pprint('--------------------------------')
 
-
 ###Retreive VLAN information from NetBox and display result in table view###
-output_vlans = requests.request("GET", url_vlans, headers=headers).json()
-n_vlans = len(output_vlans['results'])
-pprint(f'The VLAN number is: {n_vlans}')
+url_vlans = "http://192.168.246.130:8000/api/ipam/vlans/"
+pprint('GENERATING VLAN TABLE')
+vlans = rest(url_vlans)
+n_vlans = len(vlans['results'])
+pprint(f'number of VLANs defined in NetBox: {n_vlans}')
 table_vlans = Table(title="VLANs")
-table_vlans.add_column("VLAN ID", justify="center", style="cyan", no_wrap=True)
+table_vlans.add_column("VID", justify="center", style="cyan", no_wrap=True)
 table_vlans.add_column("NAME", style="magenta")
 table_vlans.add_column("DESCRIPTION", style="green")
 table_vlans.add_column("SITE", justify="center", style="blue")
 for i in range (n_vlans):
-  table_vlans.add_row(str(output_vlans['results'][i]['vid']), output_vlans['results'][i]['name'], output_vlans['results'][i]['description'], output_vlans['results'][i]['site']['name'])
+  table_vlans.add_row(str(vlans['results'][i]['vid']), vlans['results'][i]['name'], vlans['results'][i]['description'], vlans['results'][i]['site']['name'])
 console = Console()
 console.print(table_vlans)
-
-pprint('GENERSTING VLAN LISTS')
+pprint('GENERATING VLAN LISTS')
 vlans_id_list = []
 vlans_name_list = []
 for vid,vname in zip(range(n_vlans),range(n_vlans)):
-    vlans_id_list.append(output_vlans['results'][vid]['vid'])
-    vlans_name_list.append(output_vlans['results'][vname]['name'])
+    vlans_id_list.append(vlans['results'][vid]['vid'])
+    vlans_name_list.append(vlans['results'][vname]['name'])
 print(vlans_id_list)
 print(vlans_name_list)
 pprint('--------------------------------')
 
 ###IP address availability scan###
-# pprint('SCANNING FOR IP ADDRESS AVAILABILITY')
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# n = n_devices
-# for num in range (n):
-#     srv_ip = ip_addr_list[num]
-#     ping = os.system('ping -c 1 ' + srv_ip)
-#     if ping == 0:
-#         print('server is up', srv_ip)
-#     else:
-#         print('server is down', srv_ip)
+pprint('SCANNING FOR IP ADDRESS AVAILABILITY')
+for ip in ip_addr_list:
+    pingresult = check_availability(ip)
+    print(pingresult)
 
 ###Define admin credentials###
 pprint('PROVIDE ADMIN CREDENTIALS')
 username = input('login: ')
 password = input('password: ')
 
-###Create VLANs on 5 switches###
+###Create VLANs on switches###
 pprint('PUSH VLAN CONFIGURATION')
 for ip in ip_addr_list:
     for v,n in zip(vlans_id_list,vlans_name_list):
-        device = ConnectHandler(device_type='cisco_ios', ip=ip, username=username, password=password)
-        vlancli = [f'vlan {v}', f'name {n}']
-        device.send_config_set(vlancli)
-        device.disconnect()
+        push_config_vlan(ip,v,n,username,password)
 
 ###Assign VLANs on 5 switches###
 pprint('PUSH SWITCHPORT CONFIGURATION')
 intf = ['gi0/2', 'gi0/3']
-for gi in intf:
+for int in intf:
     for ip in ip_addr_list:
-        device = ConnectHandler(device_type='cisco_ios', ip=ip, username=username, password=password)
-        portcli = [f'interface {gi}', 'description **SPB-INFRASTRUCTURE cfg by netmiko**', 'switchport mode access', f'switchport access vlan 104']
-        device.send_config_set(portcli)
-        device.disconnect()
-        
+        push_config_switchport(ip,int,username,password)
+
 ##Verify###
-sw_ip = str(input('SWITCH IP: '))
-pprint(f'DISPLAY CONFIGURATION FOR {sw_ip}')
-device = ConnectHandler(device_type='cisco_ios', ip=sw_ip, username=username, password=password)
-showvlanbri = device.send_command('show vlan bri')
-print(showvlanbri)
-showswpcfg = device.send_command('show run int gi0/2')
-print(showswpcfg)
-device.disconnect()
+# ip = str(input('SWITCH IP: '))
+ip = '192.168.246.242'
+pprint(f'DISPLAY CONFIGURATION FOR {ip}')
+commands = ['sh vlan bri', 'sh run int gi0/3']
+for cmd in commands:
+    device = ConnectHandler(device_type='cisco_ios', ip=ip, username=username, password=password)
+    output = device.send_command(cmd)
+    print(output)
