f2c2670d7fc526a6c81023396d5dfaef9a5506e6
ialshaev <alshaev51@gmail.com>
2022-03-07 00:36:41 +0300

    flush repo

---
+++ pexpect_multiprocessing.py
@@ -0,0 +1,37 @@
+import pexpect
+from inventory import devices
+from multiprocessing import Process
+from time import perf_counter
+import getpass
+
+def send_cmd (device,name,cmd,login,password):
+    ssh = pexpect.spawn('ssh {}@{}'.format(login,device))
+    ssh.expect('[pP]assword')
+    ssh.sendline(password)
+    ssh.expect('[>#]')
+    ssh.sendline(cmd)
+    ssh.expect('{}#'.format(name))
+    output = ssh.before.decode('utf-8')
+    print(output)
+
+if __name__ == "__main__":
+    cmd = input('enter the command: ')
+    username = input("login: ")
+    password = getpass.getpass()
+    processes = []
+    start = perf_counter()
+    for d in devices:
+        device = d['host']
+        name = d['hostname']
+        proc = Process(target=send_cmd, args=(device,name,cmd,username,password))
+        processes.append(proc)
+
+    for proc in processes:
+        proc.start()
+
+    for proc in processes:
+        proc.join()
+
+    end = perf_counter()
+    total_time = end - start
+    print(total_time)
