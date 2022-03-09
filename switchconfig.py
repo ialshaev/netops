590bffba9066e58f6783b642e4c9d84c86dc4987
ialshaev <alshaev51@gmail.com>
2022-03-09 18:43:39 +0300

    flush the code

--- switchconfig.py
+++ switchconfig.py
@@ -39,27 +39,7 @@ def check_availability(ip):
         pingresult = ip + ' is Unavailable'
     return(pingresult)
 
-if __name__ == "__main__":
-    ###Retreive device information from NetBox###
-    pprint('RETREIVING DEVICE INFORMATION')
-    url_devices = "http://192.168.246.130:8000/api/dcim/devices/"
-    devices = rest(url_devices)
-    n_devices = len(devices['results'])
-    pprint(f'number of devices defined in NetBox: {n_devices}')
-    pprint('GENERATING IP ADDRESS LIST') # create the list of IP addresses
-    ip_addr_list = []
-    for i in range (n_devices):
-        ip = devices['results'][i]['primary_ip4']['address']
-        ip_addr_list.append(ip[:-3])
-    pprint(f'The list of ip addresses: {ip_addr_list}') # pprint(type(ip_addr_list[0])) --> check for the list element type, must be string
-    pprint('--------------------------------')
-
-    ###Retreive VLAN information from NetBox and display result in table view###
-    url_vlans = "http://192.168.246.130:8000/api/ipam/vlans/"
-    pprint('GENERATING VLAN TABLE')
-    vlans = rest(url_vlans)
-    n_vlans = len(vlans['results'])
-    pprint(f'number of VLANs defined in NetBox: {n_vlans}')
+def create_table(n_vlans,vlans):
     table_vlans = Table(title="VLANs")
     table_vlans.add_column("VID", justify="center", style="cyan", no_wrap=True)
     table_vlans.add_column("NAME", style="magenta")
@@ -69,7 +49,29 @@ if __name__ == "__main__":
         table_vlans.add_row(str(vlans['results'][i]['vid']), vlans['results'][i]['name'], vlans['results'][i]['description'], vlans['results'][i]['site']['name'])
     console = Console()
     console.print(table_vlans)
-    pprint('GENERATING VLAN LISTS')
+
+if __name__ == "__main__":
+    pprint('STEP1: RETREIVING DEVICE INFORMATION')
+    url_devices = "http://192.168.246.130:8000/api/dcim/devices/"
+    devices = rest(url_devices) #Retreive device information from NetBox
+    n_devices = len(devices['results'])
+    # pprint(f'number of devices defined in NetBox: {n_devices}')
+
+    pprint('STEP2: GENERATING IP ADDRESS LIST') # create the list of IP addresses
+    ip_addr_list = []
+    for i in range (n_devices):
+        ip = devices['results'][i]['primary_ip4']['address']
+        ip_addr_list.append(ip[:-3])
+    pprint('The list of ip addresses: %s'%ip_addr_list) # pprint(type(ip_addr_list[0])) --> check for the list element type, must be string
+
+    pprint('STEP3: GENERATING VLAN TABLE')
+    url_vlans = "http://192.168.246.130:8000/api/ipam/vlans/"
+    vlans = rest(url_vlans) #Retreive VLAN information from NetBox and display result in table view
+    n_vlans = len(vlans['results'])
+    # pprint(f'number of VLANs defined in NetBox: {n_vlans}')
+    create_table(n_vlans,vlans)
+
+    pprint('STEP4: GENERATING VLAN LISTS')
     vlans_id_list = []
     vlans_name_list = []
     for vid,vname in zip(range(n_vlans),range(n_vlans)):
@@ -77,43 +79,36 @@ if __name__ == "__main__":
         vlans_name_list.append(vlans['results'][vname]['name'])
     print(vlans_id_list)
     print(vlans_name_list)
-    pprint('--------------------------------')
 
-    ###IP address availability scan###
     pprint('SCANNING FOR IP ADDRESS AVAILABILITY')
     for ip in ip_addr_list:
-        res = check_availability(ip)
+        res = check_availability(ip) #IP address availability scan
         print(res)
 
-    ###Define admin credentials###
-    pprint('PROVIDE ADMIN CREDENTIALS')
+    pprint('PROVIDE ADMIN CREDENTIALS') #Define admin credentials
     username = input('Login: ')
     password = getpass.getpass()
 
-    ###Create VLANs on switches###
     pprint('PUSH VLAN CONFIGURATION')
     process1 = []
     for v,n in zip(vlans_id_list,vlans_name_list):
         for ip in ip_addr_list:
-            proc = Process(target=push_config_vlan, args=(ip,v,n,username,password))
+            proc = Process(target=push_config_vlan, args=(ip,v,n,username,password)) #Create VLANs on switches
             process1.append(proc)
             proc.start()
         proc.join()
 
-    ###Assign VLANs on 5 switches###
     pprint('PUSH SWITCHPORT CONFIGURATION')
     process2 = []
     intf = ['gi0/2', 'gi0/3']
     for int in intf:
         for ip in ip_addr_list:
-            proc = Process(target=push_config_switchport, args=(ip,int,username,password))
+            proc = Process(target=push_config_switchport, args=(ip,int,username,password)) #Assign VLANs on 5 switches
             process2.append(proc)
             proc.start()
         proc.join()
 
-    ##Verify###
-    # ip = str(input('SWITCH IP: '))
-    ip = '192.168.246.242'
+    ip = str(input('SWITCH IP: ')) #Config verification
     pprint(f'DISPLAY CONFIGURATION FOR {ip}')
     commands = ['sh vlan bri', 'sh run int gi0/3']
     for cmd in commands:
